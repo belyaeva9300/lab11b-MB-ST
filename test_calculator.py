14b2c17f00084a52b4e09fe0763944794b396767
belyaeva <masha.bel2007@gmail.com>
2026-04-13 17:31:56 -0400

    more merges

--- test_calculator.py
+++ test_calculator.py
@@ -16,12 +16,16 @@ class TestCalculator(unittest.TestCase):
     ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(0, 5), 0)
+        self.assertEqual(mul(-2, 3), -6)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        self.assertEqual(div(2, 10), 5)
+        self.assertEqual(div(3, 9), 3)
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
     ######## Partner 2
     def test_divide_by_zero(self):  # 1 assertion
@@ -39,21 +43,20 @@ class TestCalculator(unittest.TestCase):
     ##########################
 
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            log( 10,-1)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(hypotenuse(5, 12), 13.0)
+        self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
+        self.assertAlmostEqual(hypotenuse(0, 5), 5.0)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            square_root(-1)
+        self.assertEqual(square_root(9), 3.0)
+        self.assertEqual(square_root(4), 2.0)
     ##########################
 
 # Do not touch this
